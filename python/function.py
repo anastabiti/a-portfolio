a6bc28db1272769fb7d7732588c9ccf4f8d37c11
anastabiti <anastabiti@gmail.com>
2024-02-06 10:39:40 +0100

    use python

--- python/function.py
+++ python/function.py
@@ -7,6 +7,15 @@ def loop_function():
     while i < 10:
         print("i = " , i)
         i+=1
+def for_loop():
+    names =["cnas", "bhmed","amine"]
+    myname = "anas"
+    names.sort()
+    for one in names:
+        print("fruit name is ",one)
+    for char in myname:
+        print("[" , char,"]")
 a_function() #we called here
 function_with_args(13,37)
 loop_function()
+for_loop()
