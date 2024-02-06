e2b7f2be0fb8e71334a296c8cc58013efcb4efd3
anastabiti <anastabiti@gmail.com>
2024-02-06 10:29:43 +0100

    use python

---
+++ python/function.py
@@ -0,0 +1,12 @@
+def a_function():
+    print(" i am a function")
+def function_with_args(x,y):
+    print("x ",x  ," y " , y)
+def loop_function():
+    i =0
+    while i < 10:
+        print("i = " , i)
+        i+=1
+a_function() #we called here
+function_with_args(13,37)
+loop_function()
