2d397024ce2c9af482f7ad09e700bfdc89702e60
anastabiti <anastabiti@gmail.com>
2024-02-08 14:34:37 +0100

    POST /user create a user

--- Djnago/ecommerce/app/urls.py
+++ Djnago/ecommerce/app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.home),
     path("auth", views.post_auth),
+    path("user", views.create_user),
 ]
--- Djnago/ecommerce/app/views.py
+++ Djnago/ecommerce/app/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
-    
+# create a user
+from django.contrib.auth.models import User
+
 def home(request):
     return HttpResponse ("WELCOME TO E-COMMERCE")
 def post_auth(request):
     print("post auth is called")
     return HttpResponse(request.method)
+def create_user(request):
+    if(request.method == 'POST'):
+        user =User.objects.create_user(request.POST.get("username") ,request.POST.get("email"),request.POST.get("password"))
+        user.save()
+    return HttpResponse(request.POST)
+    
