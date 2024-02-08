3b110b242992fcf32adad6b9460189ff3701f56e
anastabiti <anastabiti@gmail.com>
2024-02-08 13:41:48 +0100

    d

--- Djnago/ecommerce/app/views.py
+++ Djnago/ecommerce/app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse ("WELCOME TO E-COMMERCE")
 def post_auth(request):
+    print("post auth is called")
     return HttpResponse(request.method)
