b371c5d370309e605edf16fe8b79d75113b559fa
anastabiti <anastabiti@gmail.com>
2024-02-27 13:22:00 +0100

    use react and vite

--- Frontend/myapp/src/App.tsx
+++ Frontend/myapp/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+        <img src='/mylogo.png' className='mylogo'></img>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -25,7 +26,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs" style={{color:"blue"}}>
         Click on the Vite and React logos to learn more
       </p>
     </>
