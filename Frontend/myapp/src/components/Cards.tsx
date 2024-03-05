58673292ea6a95483ad21ccba12bf5bbafe9db6f
anastabiti <anastabiti@gmail.com>
2024-03-05 13:17:52 +0100

    update front

--- Frontend/myapp/src/components/Cards.tsx
+++ Frontend/myapp/src/components/Cards.tsx
@@ -1,11 +1,10 @@
-export function Cards()
-{   
-    return (
-        <div className="cards">
-            
-        <h2 style={{position:"absolute", top:"10px"}}> 
-        Welcome <span style={{color:"white"}}>Deals</span>
-        </h2>    
-        </div>
-    );
+export function Cards() {
+  return (
+    <div className="cards">
+      <h2 style={{ position: "absolute", top: "10px" }}>
+        Welcome <span style={{ color: "white" }}>Deals</span>
+      </h2>
+      <img src="OVER.gif" width={300} height={500} style={{marginLeft:"-23%"}}></img>
+    </div>
+  );
 }
