6c53052e671d10019ca979364f1bc1cdec5e1a9e
anastabiti <anastabiti@gmail.com>
2024-03-06 11:20:49 +0100

    update front

--- Frontend/myapp/src/components/Card1.tsx
+++ Frontend/myapp/src/components/Card1.tsx
@@ -2,8 +2,10 @@ export function Card1()
 {
     return (
         <div className="card1">
-            
-        <img src="laptop.png" width={250} height={200} style={{float:"right", overflowX:"auto"}}></img>    
+        <h1 style={{fontFamily:"Poppins",color:"white",position:"absolute"}}>$1999</h1>
+        <img src="laptop.png" width={250} height={220} style={{float:"right", overflowX:"auto"}}></img>   
+        <button style={{position:'absolute',top:"80%", background:"white", borderRadius:"20px",
+width:"120px",height:"55px"}}> <h4 style={{color:"red",fontFamily:"Poppins"}}>Add to cart</h4> </button>
         </div>
     );
 }
--- Frontend/myapp/src/components/Cards.tsx
+++ Frontend/myapp/src/components/Cards.tsx
@@ -4,7 +4,7 @@ export function Cards() {
       <h2 style={{ position: "absolute", top: "10px" }}>
         Welcome <span style={{ color: "white" }}>Deals</span>
       </h2>
-      <img src="OVER.gif" width={300} height={500} style={{marginLeft:"-23%"}}></img>
+      {/* <img src="OVER.gif" width={300} height={500} style={{marginLeft:"-23%"}}></img> */}
     </div>
   );
 }
